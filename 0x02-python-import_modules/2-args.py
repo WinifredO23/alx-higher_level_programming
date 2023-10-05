b906d1b1e33172fcfa074761eddae9e2946d52b5
WinifredO23 <WinifredOnumbu@outlook.com>
2023-10-05 06:18:30 -0700

    arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(argc))
 
     for i in range(argc):
-         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
